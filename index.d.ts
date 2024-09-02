f05a11b1454d6f97d87979bda0d25b21eb1a49d9
Richard Gigi <gigirichardofficial@gmail.com>
2024-09-02 16:49:20 +0100

    NPM package 1.0.3

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,13 @@
 declare module '@richaadgigi/stylexui' {
-    function findElementWithAttribute(
-        element: Element | null,
-        attributeName: string
-    ): Element | null;
-    
-    function isHidden(el: Element): boolean;
     function xuiLoadingScreen(): void;
     function xuiHideSkeleton(ele: string | null): void;
     function xuiModal(): void;
     function xuiAccordion(): void;
     function xuiAlerts(): void;
     function xuiLazyLoadings(): void;
-    function xuiAnime(customDefinition: string | undefined): void;
-    function xuiAnimeStart(customDefinition: string | undefined): void;
-    function xuiAnimeEnd(customDefinition: string | undefined): void;
+    function xuiAnime(customDefinition?: string): void;
+    function xuiAnimeStart(customDefinition?: string): void;
+    function xuiAnimeEnd(customDefinition?: string): void;
     function xuiTypeWriter(obj: {
         words: string[],
         duration?: number,
